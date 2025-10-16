3dd464568c19272b01ba9f05ac558424dac6bffd
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2025-10-16 13:02:04 +0500

    Add annotation to MODEL_REGISTRY values. Now - nn.Module

--- app/model_registry.py
+++ app/model_registry.py
@@ -1,10 +1,11 @@
 from typing import Dict, Type
+from torch.nn import Module
 
 from app.services.models_classes.cnn_model import BetterCNN
 from app.services.models_classes.resnet18_model import ResNET18
 
 # Поддерживаемые модели = название модели: класс
-MODEL_REGISTRY: Dict[str, Type] = {
+MODEL_REGISTRY: Dict[str, Type[Module]] = {
     "cnn": BetterCNN,
     "resnet": ResNET18,
 }
